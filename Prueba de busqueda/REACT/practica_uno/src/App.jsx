6d92343e483a2e1251df0e03ec6eb4ea011dc93b
MoisesCenteno8 <moises.centeno57@gmail.com>
2023-06-20 01:26:45 -0600

    cambio

---
+++ Prueba de busqueda/REACT/practica_uno/src/App.jsx
@@ -0,0 +1,16 @@
+//importando el css
+import './assets/css/style.css'
+
+
+function App() {
+  const nombre = "Moises Centeno";
+  return (
+    //siempre tiene que haber un contenedor padre
+    <>
+      <h1>Bienvenido {nombre} a mi primera practica</h1>
+      <p>Estamos trabajo con react</p>
+    </>
+  )
+}
+
+export default App
